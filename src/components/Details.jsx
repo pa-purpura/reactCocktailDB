4ca5066009de8bd199864e461811880f8c57270c
pa-purpura <p.purpura@outlook.fr>
2021-12-23 19:25:00 +0100

    details cocktail page

---
+++ src/components/Details.jsx
@@ -0,0 +1,95 @@
+import { useState } from 'react';
+import '../assets/styles/colors.css';
+
+function Details({ props }) {
+
+    const [language, setLanguage] = useState('english')
+
+
+    const ingredients = [];
+    // console.log('prima', ingredients)
+
+    for (const key in props) {
+        if (!!props[key] && key.includes('strIngredient')) {
+            console.log(`${key}: ${props[key]}`);
+            ingredients.push(props[key])
+        }
+
+    }
+
+
+
+    // console.log('dopo', ingredients)
+    return (
+        <>
+            <div className="col-md-4 p-2">
+                <div className="card bg_white">
+                    <img src={props.strDrinkThumb} className="card-img-top" alt={props.strDrink} />
+                    <div className="card-body">
+                        <h2 className="card-title">{props.strDrink}</h2>
+                        {/* <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> */}
+                    </div>
+                    <ul className="list-group list-group-flush">
+                        <li className="list-group-item">Category: {props.strCategory}</li>
+                        <li className="list-group-item">Glass: {props.strGlass}</li>
+                        <li className="list-group-item">{props.strAlcoholic}</li>
+                    </ul>
+                    <div className="card-body">
+                        {props.strInstructionsDE
+                            ? <a onClick={() => { setLanguage('german') }} className="card-link">🇩🇪</a>
+                            : null
+                        }
+                        {props.strInstructionsIT
+                            ? <a onClick={() => { setLanguage('italian') }} className="card-link">🇮🇹</a>
+                            : null
+                        }
+                        {props.strInstructionsES
+                            ? <a onClick={() => { setLanguage('espagnol') }} className="card-link">🇪🇸</a>
+                            : null
+                        }
+                        {props.strInstructionsFR
+                            ? <a onClick={() => { setLanguage('franch') }} className="card-link">🇫🇷</a>
+                            : null
+                        }
+                    </div>
+                </div>
+            </div>
+            <div className="col-md-8 p-2">
+                <div>
+                    <h4>Ingredients: </h4>
+                    <ul>
+                        {ingredients.map(item => <li>{item}</li>)}
+                    </ul>
+
+                </div>
+                <div>
+                    <h4>Instructions: </h4>
+                    {
+                        (() => {
+                            switch (language) {
+                                case "italian":
+                                    return <p>{props.strInstructionsIT}</p>
+                                    break;
+                                case "german":
+                                    return <p>{props.strInstructionsDE}</p>;
+                                    break;
+                                case "french":
+                                    return <p>{props.strInstructionsFR}</p>;
+                                    break;
+                                case "espagnol":
+                                    return <p>{props.strInstructionsES}</p>;
+                                    break;
+                                default: return <p>{props.strInstructions}</p>;
+                                    break;
+                            }
+                        }).call(this)
+                    }
+                </div>
+
+
+            </div>
+        </>
+    )
+}
+
+export default Details
--- src/pages/Cocktail.jsx
+++ src/pages/Cocktail.jsx
@@ -1,9 +1,10 @@
 import { useEffect } from "react"
 import { useParams } from "react-router-dom"
 import useSearch from '../hooks/useSearch';
-import { useContext, useState } from 'react';
+import { useContext } from 'react';
 import { SearchContext } from '../context/Search';
 import '../assets/styles/colors.css';
+import Details from "../components/Details";
 
 function Cocktail() {
 
@@ -11,9 +12,9 @@ function Cocktail() {
     const search = useSearch()
     const context = useContext(SearchContext)
     const drink = context.cocktail
-    console.log(drink.strDrink)
+    // console.log(drink.strDrink)
 
-    console.log('pronto ad essere renderizzato', drink)
+    // console.log('pronto ad essere renderizzato', drink)
 
     useEffect(() => {
         search.getDrink(id)
@@ -23,7 +24,11 @@ function Cocktail() {
     return (
         drink.map(el => {
             return (
-                <h4>cocktail details of id:{el.idDrink} and name: {el.strDrink} works!</h4>
+                <div className="container">
+                    <div className="row m-5 ">
+                        <Details props={el} />
+                    </div>
+                </div>
             )
         })
 
@@ -31,3 +36,4 @@ function Cocktail() {
 }
 
 export default Cocktail
+
